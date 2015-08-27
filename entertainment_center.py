0500520fad8ce24f306d2335eeaa2e744ec821d9
Scott Harman <scott@harman.tv>
2015-08-27 21:41:41 +1000

    added various elements and a few visual tweaks

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,27 @@
 import media
+import fresh_tomatoes
 
 aliens = media.Movie("Aliens",
                      "Stranded on a hostile environment, the crew of the Nostromo struggle for survival against an unknown force",
-                     "dunno",
-                     "youtube.com")
+                     "https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Aliens_poster.jpg/220px-Aliens_poster.jpg",
+                     "https://www.youtube.com/watch?v=zNE0dlHcmgA",
+                     "Sigourney Weaver",
+                     "4.8 Xenomorphs")
 
-print(aliens.storyline)
-print (aliens.to_string())
+jurassic_world = media.Movie("Jurassic World",
+                              "Once again, someone tries to help life find a way, and Starlord has to step in to save the day",
+                              "https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/Jurassic_World_poster.jpg/220px-Jurassic_World_poster.jpg",
+                              "https://www.youtube.com/watch?v=RFinNxS5KN4",
+                              "Chris Pratt (Starlord)",
+                              "Waiting for it to come out on Bluray")
+
+terminator = media.Movie("Terminator: Genisys",
+                          "Damn, showing my age now - I remember when this came out the first time.",
+                          "https://upload.wikimedia.org/wikipedia/en/thumb/b/bc/Terminator_Genisys.JPG/220px-Terminator_Genisys.JPG",
+                          "https://www.youtube.com/watch?v=jNU_jrPxs-0",
+                          "Arnie - he'll be back.  I'm sure of it.",
+                          "T-1000")
+
+movies_array = [aliens,jurassic_world,terminator]
+fresh_tomatoes.open_movies_page(movies_array)
+
--- media.py
+++ media.py
@@ -1,15 +1,17 @@
 import webbrowser
 
+# SAH Added starring and rating to default movie class
 class Movie():
-    def __init__(self,movie_title,movie_storyline,poster_url,trailer_youtube):
+    def __init__(self,movie_title,movie_storyline,poster_url,trailer_youtube, starring, rating):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_url
-        self.trailer_url = trailer_youtube
+        self.trailer_youtube_url = trailer_youtube
+        self.starring = starring
+        self.rating = rating
 
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
 
-
-    def to_string(self):
-        return self.title + ": " + self.storyline
+#    def to_string(self):
+#        return self.title + ": " + self.storyline
